baf2883229fd60882bee8cb7c3cef4c65aee1435
Timothee Groleau <timothee.groleau@gmail.com>
2020-05-30 23:45:23 +0800

    2 more solutions

--- exercises/queen-attack/queen-attack.js
+++ exercises/queen-attack/queen-attack.js
@@ -1,18 +1,28 @@
-//
-// This is only a SKELETON file for the 'Queen Attack' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class QueenAttack {
-    constructor() {
-        throw new Error("Remove this statement and implement this function");
-    }
+	constructor({ white = [0, 3], black = [7, 3] } = {}) {
+		this.white = white;
+		this.black = black;
 
-    toString() {
-        throw new Error("Remove this statement and implement this function");
-    }
+		if (white[0] == black[0] && white[1] == black[1]) {
+			throw new Error('Queens cannot share the same space');
+		}
+	}
 
-    canAttack() {
-        throw new Error("Remove this statement and implement this function");
-    }
+	toString() {
+		const board = Array(8).fill().map(_ => Array(8).fill('_'));
+
+		board[this.white[0]][this.white[1]] = 'W';
+		board[this.black[0]][this.black[1]] = 'B';
+
+		return board.map(row => row.join(' ') + '\n').join('');
+	}
+
+	canAttack() {
+		const dx = this.white[0] - this.black[0];
+		const dy = this.white[1] - this.black[1];
+
+		return dx == 0
+			|| dy == 0
+			|| Math.abs(dx) == Math.abs(dy);
+	}
 }
--- exercises/sieve/sieve.js
+++ exercises/sieve/sieve.js
@@ -1,8 +1,37 @@
-//
-// This is only a SKELETON file for the 'Sieve' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const primes = (n) => {
+	// compute list of primes up to n,
+	// using the Sieve of Eratosthenes
+	// https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 
-export const primes = () => {
-  throw new Error("Remove this statement and implement this function");
+	const list = Array(n).fill().map((_, idx) => idx + 1);
+
+	if (n < 2) return [];
+
+	function getFirstNonNullAfter(idx) {
+		do {
+			if (list[idx++] !== null) return idx;
+		}
+		while(idx < list.length)
+	}
+
+	function clearMultiplesOf(n) {
+		let multiplier = 2;
+
+		while (n * multiplier <= list.length) {
+			list[n * multiplier - 1] = null;
+			multiplier++;
+		}
+	}
+
+	let walk = 1;
+
+	while (walk < list.length) {
+		const base = getFirstNonNullAfter(walk);
+
+		clearMultiplesOf(base);
+
+		walk = base;
+	}
+
+	return list.filter(_ => _).slice(1);
 };
