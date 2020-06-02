0037c2ceae0405d3bb801d563e0c3b3d3eab6898
Timothee Groleau <tgroleau@zendesk.com>
2020-06-02 10:37:16 +0800

    More solutions

--- exercises/all-your-base/all-your-base.js
+++ exercises/all-your-base/all-your-base.js
@@ -1,8 +1,31 @@
-//
-// This is only a SKELETON file for the 'All Your Base' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const convert = (in_digits, in_base=-1, out_base=-1) => {
+	if (in_base <= 1 || in_base % 1) {
+		throw new Error('Wrong input base')
+	}
 
-export const convert = () => {
-  throw new Error("Remove this statement and implement this function");
+	if (out_base <= 1 || out_base % 1) {
+		throw new Error('Wrong output base')
+	}
+
+	if (
+		in_digits.length <= 0
+		|| in_digits.some(d => d < 0 || d >= in_base)
+		|| (in_digits.length > 1 && in_digits[0] == 0)
+	) {
+		throw new Error('Input has wrong format')
+	}
+
+	let num = in_digits
+		.reverse()
+		.reduce((sum, v, idx) => sum + v * Math.pow(in_base, idx), 0);
+
+	const out_digits = [];
+
+	do {
+		out_digits.unshift(num % out_base);
+		num = Math.floor(num / out_base);
+	}
+	while(num);
+
+	return out_digits;
 };
--- exercises/yacht/yacht.js
+++ exercises/yacht/yacht.js
@@ -1,8 +1,75 @@
-//
-// This is only a SKELETON file for the 'Yacht' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+function by_num(a, b) {
+	return a - b;
+}
 
-export const score = () => {
-  throw new Error("Remove this statement and implement this function");
+function valueCounts(dice) {
+	return dice.reduce((acc, v) => {
+		const count = acc.get(v);
+		acc.set(v, (count || 0) + 1);
+		return acc;
+	}, new Map());
+}
+
+const SCORERS = {};
+
+// dynamic methods
+['ones', 'twos', 'threes', 'fours', 'fives', 'sixes'].forEach((method, idx) => {
+	const num = idx + 1;
+
+	SCORERS[method] = dice => num * dice.filter(v => v === num).length;
+});
+
+SCORERS['full house'] = dice => {
+	let score = 0;
+
+	for (let [value, count] of valueCounts(dice).entries()) {
+		if (count === 2 || count === 3) {
+			score += value * count;
+			continue;
+		}
+
+		return 0;
+	}
+
+	return score;
+};
+
+SCORERS['four of a kind'] = dice => {
+	for (let [value, count] of valueCounts(dice).entries()) {
+		if (count === 5 || count == 4) return value * 4;
+	}
+
+	return 0;
+};
+
+SCORERS['yacht'] = dice => {
+	for (let count of valueCounts(dice).values()) {
+		if (count === 5) return 50;
+
+		return 0;
+	}
+};
+
+SCORERS['little straight'] = dice => {
+	return SCORERS['big straight'](dice, 1);
+}
+
+SCORERS['big straight'] = (dice, first=2) => {
+	dice.sort(by_num);
+
+	if (dice[0] !== first) return 0;
+
+	for (let idx=dice.length; idx-- > 1; ) {
+		if (dice[idx] - dice[idx-1] != 1) return 0;
+	}
+
+	return 30;
+}
+
+SCORERS['choice'] = dice => {
+	return dice.reduce((sum, v) => sum + v, 0);
+}
+
+export const score = (dice, category) => {
+	return SCORERS[category](dice);
 };
