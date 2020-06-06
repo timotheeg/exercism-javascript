09d7b58b25c49ad18f891dfbee5cdf90aa6b513a
Timothee Groleau <timothee.groleau@gmail.com>
2020-06-07 00:23:43 +0800

    2 more solutions

--- exercises/meetup/meetup.js
+++ exercises/meetup/meetup.js
@@ -1,8 +1,50 @@
-//
-// This is only a SKELETON file for the 'Meetup' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const QUALIFIERS = {
+	'first':  1,
+	'second': 2,
+	'third':  3,
+	'fourth': 4,
+	'fifth':  5,
+};
+const DAY = {
+	'Monday':    1,
+	'Tuesday':   2,
+	'Wednesday': 3,
+	'Thursday':  4,
+	'Friday':    5,
+	'Saturday':  6,
+	'Sunday':    0,
+}
 
-export const meetup = () =>  {
-  throw new Error("Remove this statement and implement this function");
+export const meetup = (year, month, qualifier, day) =>  {
+	const dow = DAY[day];
+	let d;
+
+	if (qualifier == 'last') {
+		d = new Date(year, month, 1);
+
+		do {
+			d.setDate(d.getDate() - 1);
+		}
+		while(d.getDay() != dow);
+	}
+	else if (qualifier == 'teenth') {
+		d = new Date(year, month - 1, 13);
+
+		while(d.getDay() != dow) {
+			d.setDate(d.getDate() + 1);
+		}
+
+		return d;
+	}
+	else {
+		d = new Date(year, month - 1, 1);
+
+		while(d.getDay() != dow) {
+			d.setDate(d.getDate() + 1);
+		}
+
+		d.setDate(d.getDate() + 7 * (QUALIFIERS[qualifier] - 1));
+	}
+
+	return d;
 }
